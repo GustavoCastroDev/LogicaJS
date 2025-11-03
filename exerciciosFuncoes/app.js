99d923a038e08951556d2a13ed005b440e5d89e9
Gustavo de Castro <122317194+GustavoCastroDev@users.noreply.github.com>
2025-11-02 21:39:59 -0300

    Finalizando as funções

--- exerciciosFuncoes/app.js
+++ exerciciosFuncoes/app.js
@@ -12,19 +12,66 @@ function mensagemNome() {
     const nome = inputNome.value;
 
     // Criando bloco de condição para garantir que o usuário preencha o input
-    if(inputNome != ""){ // Se 'inputNome' for diferente de vazio ("")
+    if (inputNome != "") { // Se 'inputNome' for diferente de vazio ("")
         console.log(`Olá, ${nome}!`); // Exibe a mensagem no console com o nome do usuário
-    } else{ 
+    } else {
         console.log('Preencha o campo com o seu nome!'); // Exibe uma mensagem no console
     }
 }
 
 // Função que recebe um número como parâmetro e retorna o dobro desse número
-function calcularDobro(numero){
-    let inputNumeroDbro = parseInt(document.getElementById('lblDobro'));
+function calcularDobro(numero) {
+    // Criando variável que armazena o valor inserindo no input do ID 'lblDobro'
+    let inputNumeroDobro = document.getElementById('lblDobro').value;
 
-    const numero = inputNumeroDbro * inputNumeroDbro;
+    // Criando variável 'numero' sem o 'let' (pois já foi declarada), que recebe o valor do input e a converte para int, além de elevar ao quadrado
+    numero = parseInt(inputNumeroDobro * 2);
 
-    console.log(numero);
+    // Exibindo o número no input
+    console.log(`Dobro do número: ${numero}`);
+}
 
+// Função que recebe três números como parâmetros para calcular a média entre eles
+function calcularMedia(numeroUm, numeroDois, numeroTres) {
+    // Criando variável que armazena o valor inserido no input por meio dos IDs
+    let inputNumUm = parseInt(document.getElementById('lblNumeroUm').value);
+    let inputNumDois = parseInt(document.getElementById('lblNumeroDois').value);
+    let inputNumTres = parseInt(document.getElementById('lblNumeroTres').value);
+
+    // Criando variável que armazena a soma dos três valores inseridos nos inputs e a convertendo
+    let mediaNumeros = (inputNumUm + inputNumDois + inputNumTres) / 3;
+
+    // Exibindo a variável 'mediaNumeros' no console
+    console.log(`A média dos números: ${inputNumUm}, ${inputNumDois} e ${inputNumTres} é: ${mediaNumeros}`);
+}
+
+// Criando função que recebe dois números como parâmetros e retorna qual o maior deles
+function calcularMaior() {
+    // Criando variáveis que armazenam os valores inseridos nos inputs pelos IDs
+    let inputNumPrimeiro = document.getElementById('lblPrimeiro').value;
+    let inputNumSegundo = document.getElementById('lblSegundo').value;
+
+    // Criando variáveis que armazenam os valores inseridos nos inputs e os convertendo
+    let NumPrimeiro = parseInt(inputNumPrimeiro);
+    let NumSegundo = parseInt(inputNumSegundo);
+
+    // Criando bloco lógico para determinar qual número entre os dois é maior
+    if (NumPrimeiro > NumSegundo) {
+        console.log(`O número ${NumPrimeiro} é maior que ${NumSegundo}`);
+    } else if (NumPrimeiro < NumSegundo) {
+        console.log(`O número ${NumSegundo} é maior que ${NumPrimeiro}`);
+    } else {
+        console.log(`Os números ${NumSegundo} e ${NumPrimeiro} são iguais`);
+    }
+}
+
+// Função que recebe um número como parãmetro e retorna o quadrado dele
+function calcularQuadrado(numeroQuadrado){
+    // Criando variável que recebe o valor inserido no input pelo ID 'lblQuadrado'
+    let inputNumQuadrado = document.getElementById('lblQuadrado').value;
+
+    // Passando o valor inserido no input para variável 'numeroQuadrado'
+    numeroQuadrado = parseInt(Math.pow(inputNumQuadrado, 2));
+
+    console.log(`O número ${inputNumQuadrado} é igual a: ${numeroQuadrado}`);
 }
