cc0bd940f5d243589e24fca69c97de00b1bfd274
Gustavo de Castro Puntani <122317194+GustavoCastroDev@users.noreply.github.com>
2025-11-05 18:15:35 -0300

    continuação funções

--- desafioFuncoes/js/app.js
+++ desafioFuncoes/js/app.js
@@ -16,15 +16,36 @@ function calcularIMC(altura, peso) {
     let imc = peso / (altura * altura);
 
     // Criando bloco lógico que determina a situação de acordo com o peso
-    if (peso < 18.5) {
+    if (imc < 18.5) {
         document.getElementById('lblSituacao').value = "Abaixo do peso";
         document.getElementById('lblResultadoIMC').value = imc.toFixed(2); // '.toFixed(2)' exobe apenas duas casas decimais
-    } else if (peso > 18.5 || peso <= 24.9) {
+    } else if (imc > 18.5 && imc <= 24.9) {
         document.getElementById('lblSituacao').value = "Peso adequado";
         document.getElementById('lblResultadoIMC').value = imc.toFixed(2); // '.toFixed(2)' exobe apenas duas casas decimais
-    } else if (peso > 25 || peso <= 29.9) {
+    } else if (imc > 25 && imc <= 29.9) {
         document.getElementById('lblSituacao').value = "Sobrepeso";
         document.getElementById('lblResultadoIMC').value = imc.toFixed(2); // '.toFixed(2)' exobe apenas duas casas decimais
+    } else if (imc > 30 && imc <= 34.9) {
+        document.getElementById('lblSituacao').value = "Obesidade Grau 1";
+        document.getElementById('lblResultadoIMC').value = imc.toFixed(2); // '.toFixed(2)' exobe apenas duas casas decimais
+    } else if (imc > 35 && imc <= 39.9) {
+        document.getElementById('lblSituacao').value = "Obesidade Grau 2";
+        document.getElementById('lblResultadoIMC').value = imc.toFixed(2); // '.toFixed(2)' exobe apenas duas casas decimais
+    } else {
+        document.getElementById('lblSituacao').value = "Obesidade Grau 3";
+        document.getElementById('lblResultadoIMC').value = imc.toFixed(2); // '.toFixed(2)' exobe apenas duas casas decimais
+    }
+}
+
+// Criando função que retorna o fatorial de um número utilizando parâmetro
+function calcularFatorial(numFatorial){
+    inputNumDesejado = document.getElementById('lblNumFatorial').value;
+    
+    if (inputNumDesejado === 0 || inputNumDesejado === 1){
+        document.getElementById('lblFatorialResultado').value = 1;
+    } else {
+        numFatorial = inputNumDesejado * calcularFatorial(inputNumDesejado - 1);
+        document.getElementById('lblFatorialResultado').value = numFatorial;
     }
 }
 
