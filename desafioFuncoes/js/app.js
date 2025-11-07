ff6814baf2a19cd508a99a3d3a890a719159436c
Gustavo de Castro Puntani <122317194+GustavoCastroDev@users.noreply.github.com>
2025-11-07 18:43:26 -0300

    Adicionando 3 novas funções

--- desafioFuncoes/js/app.js
+++ desafioFuncoes/js/app.js
@@ -49,3 +49,52 @@ function calcularFatorial(numFatorial){
     }
 }
 
+// Criando função que converte valor em dolar para real
+function calcularDolar(real){
+    // Variável que acumula valor inserido no campo que recebe o valor em dolar
+    let lblInputDolar = document.getElementById('lblReal').value;
+
+    // Armazenando o valor inserido no parâmetro/variável da função 'real' por meio de 'lblInputDolar'
+    real = parseFloat(lblInputDolar); // Realizando conversão para Float
+
+    // Criando a variável 'dolar'
+    let dolar = real/4.8;
+
+    // Exibindo o valor convertido no input por meio do ID
+    document.getElementById('lblDolar').value = dolar;
+
+}
+
+// Criando função que calcula perímetro de retângulo com parâmetros
+function calcularPerimetro(alturaRetangulo, larguraRetangulo){
+    // Criando variáveis que armazenam os valores inseridos por meio dos IDs
+    let inputAlturaRetangulo = document.getElementById('lblAlturaRetangulo').value;
+    let inputLarguraRetangulo = document.getElementById('lblLarguraRetangulo').value;
+
+    // Armazenando os valores inseridos nos campos para os parâmetros/variáveis que a função utiliza
+    alturaRetangulo = parseFloat(inputAlturaRetangulo); // Realizando conversão para Float
+    larguraRetangulo = parseFloat(inputLarguraRetangulo); // Realizando conversão para Float
+
+    // Criando variável que armazena o valor do perímetro
+    let perimetro = (inputAlturaRetangulo * 2) + (inputLarguraRetangulo * 2);
+
+    // Exibindo o valor do perímetro no seu campo por meio do ID
+    document.getElementById('lblPerimetro').value = perimetro;
+
+}
+
+// Criando função que calcula o valor da área de um círculo utilizando parâmetro
+function calcularArea(raio){
+    // Variável que armazena o valor inserido no campo por meio do ID
+    let inputRaio = document.getElementById('lblRaio').value;
+
+    // Armazenando o valor obtido no input no parâmetro/variável que a função utiliza
+    raio = parseFloat(inputRaio); // Realizando conversão para Float
+
+    // Criando variável que armazena o valor da área do cículo
+    let areaCirculo = raio * raio * 3.14;
+
+    // Exibindo no campo o valor da área por meio do ID
+    document.getElementById('lblArea').value = areaCirculo.toFixed(2); // Exibindo apenas duas casas decimais
+}
+
